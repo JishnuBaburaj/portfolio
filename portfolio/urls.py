be2d278a1d45a00cd41c342ac644058fb3089647
JishnuBaburaj <jishnu1705@gmail.com>
2020-12-05 21:54:09 +0530

    Added home page

---
+++ portfolio/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+import jobs.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', jobs.views.home, name='home'),
+]
